58446b70eb9d3f82d462b9700fcb1bf5f47cab75
xiaohua2003 <xcai3@ualberta.ca>
2021-08-20 07:54:42 -0600

    fetch weather data

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express=require("express")
 const app =express()
-app.listen(4001, function(){
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(4000, function(){
     console.log("server is running on port 4000")
 })
+
 app.use(express.static('website'));
-app.get("/get", function(req,res){
+
+app.get("/all", function(req,res){
     res.render("../test.html")
 })
--- website/app.js
+++ website/app.js
@@ -1,9 +1,16 @@
 //set global variable
-const baseURL="api.openweathermap.org/data/2.5/weather?q="
-const apiKey="9c606971d1bdf33fab80334cc3f2c71c";
-const city=document.getElementById('city')
+const baseURL="https://api.openweathermap.org/data/2.5/weather?q="
+const apiKey="9c606971d1bdf33fab80334cc3f2c71c&units=metric";
+const city=document.getElementById('city').value
 const URL=`${baseURL}${city}&appid=${apiKey}`
 
+document.getElementById('generate').addEventListener("click", async function(){
+  const city=document.getElementById('city').value
+  const URL=`${baseURL}${city}&appid=${apiKey}`
+let weatherData= await getWeather(URL)
+
+
+})
 
 
 //get weather data
@@ -18,4 +25,21 @@ console.log(error);
   }
 }
 
-getWeather(URL)
+//post data to server
+async function postData(url, data){
+  try{
+    let res=await fetch(url,{
+      method:'Post',
+      credentials:'same-origin',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(data)
+    })
+    let data=await res.json();
+    console.log(data)
+    return data
+  }catch(error){
+    console.log(error)
+  }
+}
